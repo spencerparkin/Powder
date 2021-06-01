7d4131635a2e212541ab569862c425cb037543c7
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2021-06-01 13:37:54 -0600

    Lament that a basic grammar engine is currently something beyond my programming abilities.  Maybe I'll revisit the compiler someday and take that approach, but for now, I think that my current approach will work fine enough.

--- Source/Compiler/LanguageConstruct.h
+++ Source/Compiler/LanguageConstruct.h
@@ -14,6 +14,10 @@ namespace Powder
 	// a function of a given token list.  If this succeeds, then the token
 	// list is grammatically correct, and therefore, represents a valid program,
 	// even if the program doesn't necessarily do what the programmer intends.
+	// Note that the user of a grammar-engine to produce a syntax-tree is probably
+	// a much better approach to this problem, but it is also much more complicated.
+	// Here I'm just going for simplicity, even if it places some restrictions
+	// on the syntactic sugar or flexiblity we can allow in the language.
 	class POWDER_API LanguageConstruct
 	{
 	public:
