052d43065dc7a7cb0caafdda5dd7a1616db801b9
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2021-06-03 23:04:11 -0600

    More work on the code generator.

--- Source/Compiler/InstructionGenerator.cpp
+++ Source/Compiler/InstructionGenerator.cpp
@@ -2,6 +2,8 @@
 #include "Assembler.h"
 #include "BranchInstruction.h"
 #include "JumpInstruction.h"
+#include "LoadInstruction.h"
+#include "PushInstruction.h"
 #include "Exceptions.hpp"
 
 namespace Powder
@@ -16,8 +18,6 @@ namespace Powder
 
 	void InstructionGenerator::GenerateInstructionList(LinkedList<Instruction*>& instructionList, const Parser::SyntaxNode* syntaxNode)
 	{
-		AssemblyData::Entry entry;
-
 		if (*syntaxNode->name == "statement-list")
 		{
 			for (const LinkedList<Parser::SyntaxNode*>::Node* node = syntaxNode->childList.GetHead(); node; node = node->GetNext())
@@ -40,6 +40,8 @@ namespace Powder
 		{
 			if (syntaxNode->childList.GetCount() != 3 && syntaxNode->childList.GetCount() != 5)
 				throw new CompileTimeException("Expected \"if-statement\" in AST to have exactly 3 or 5 children.");
+
+			AssemblyData::Entry entry;
 
 			// Execute conditional instructions.  What remains on the evaluation stack top gets consumed by the branch instruction.
 			this->GenerateInstructionList(instructionList, syntaxNode->childList.GetHead()->GetNext()->value);
@@ -79,6 +81,82 @@ namespace Powder
 				entry.instruction = failInstructionList.GetHead()->value;
 				branchInstruction->assemblyData->configMap.Insert("branch", entry);
 			}
+		}
+		else if (*syntaxNode->name == "expression")
+		{
+			if (syntaxNode->childList.GetCount() != 1)
+				throw new CompileTimeException("Expected \"expression\" in AST to have exactly one child.");
+
+			this->GenerateInstructionList(instructionList, syntaxNode->childList.GetHead()->value);
+		}
+		else if (*syntaxNode->name == "binary-expression")
+		{
+			//...
+		}
+		else if (*syntaxNode->name == "unary-expression")
+		{
+			//...
+		}
+		else if (*syntaxNode->name == "literal")
+		{
+			if (syntaxNode->childList.GetCount() != 1)
+				throw new CompileTimeException("Expected \"literal\" in AST to have exactly one child.");
+
+			if (syntaxNode->childList.GetHead()->value->childList.GetCount() != 1)
+				throw new CompileTimeException("Expected \"literal\" in AST to have exactly one grandchild.");
+
+			PushInstruction* pushInstruction = Instruction::CreateForAssembly<PushInstruction>();
+			instructionList.AddTail(pushInstruction);
+
+			const Parser::SyntaxNode* literalTypeNode = syntaxNode->childList.GetHead()->value;
+			const Parser::SyntaxNode* literalDataNode = literalTypeNode->childList.GetHead()->value;
+
+			AssemblyData::Entry typeEntry;
+			AssemblyData::Entry dataEntry;
+
+			if (*literalTypeNode->name == "string-literal")
+			{
+				typeEntry.code = PushInstruction::DataType::STRING;
+				dataEntry.string = *literalDataNode->name;
+			}
+			else if (*literalTypeNode->name == "number-literal")
+			{
+				typeEntry.code = PushInstruction::DataType::NUMBER;
+				dataEntry.number = ::strtol(literalDataNode->name->c_str(), nullptr, 10);
+			}
+			else if (*literalTypeNode->name == "list-literal")
+			{
+				typeEntry.code = PushInstruction::DataType::EMPTY_LIST;
+			}
+			else
+			{
+				throw new CompileTimeException(FormatString("Did not recognize \"%s\" data-type under \"literal\" in AST.", literalTypeNode->name->c_str()));
+			}
+
+			pushInstruction->assemblyData->configMap.Insert("type", typeEntry);
+			pushInstruction->assemblyData->configMap.Insert("data", dataEntry);
+
+			if (*literalTypeNode->name == "list-literal")
+			{
+				// In this case, next come the instructions that populate the list.
+				this->GenerateInstructionList(instructionList, literalTypeNode);
+			}
+		}
+		else if (*syntaxNode->name == "identifier")
+		{
+			if (syntaxNode->childList.GetCount() != 1)
+				throw new CompileTimeException("Expected \"identifier\" in AST to have exactly one child.");
+
+			const Parser::SyntaxNode* identifierNode = syntaxNode->childList.GetHead()->value;
+			LoadInstruction* loadInstruction = Instruction::CreateForAssembly<LoadInstruction>();
+			AssemblyData::Entry entry;
+			entry.string = *identifierNode->name;
+			loadInstruction->assemblyData->configMap.Insert("name", entry);
+			instructionList.AddTail(loadInstruction);
+		}
+		else if (*syntaxNode->name == "list-literal")
+		{
+			//...
 		}
 	}
 }
