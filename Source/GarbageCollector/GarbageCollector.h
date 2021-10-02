5e0dcfee246ee891fb95d1942f2a9a1be8ba9e8c
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2021-10-02 00:20:45 -0600

    Right now, this is my current hypothesis as to why I'm seeing crashes.  It's not really a bug in the GC, but in how the GC is being used.  Still, a good system should never crash no matter how it's trying to be used, but I think there have to be some exceptions to this (e.g., alloc/free where free crashes if you hand it bad memory.)  I think I should add a debug mode to the GC that detects the re-use of objects by the GC user that the GC thought were to be freed.

--- Source/GarbageCollector/GarbageCollector.h
+++ Source/GarbageCollector/GarbageCollector.h
@@ -9,6 +9,14 @@ namespace Powder
 {
 	class GCObject;
 
+	// TODO: Detect mis-usage of the GC by marking objects as dead instead of actually deleting them.
+	//       Then, when a connection is made with a dead object, we can see that the GC user most likely
+	//       tried to transfer ownership of an object without keeping it connected to an anchor at all
+	//       times, which produces a race condition where we, if we're fast enough, can sneak in and
+	//       delete an object before ownership is transfered.  This is my current guess for why I'm seeing
+	//       some crashes where the GC is processing deleted objects.  The GC user thinks the object is
+	//       still in scope and connects it something else and that's how it ends up back in our list as
+	//       a stale object.
 	class POWDER_API GarbageCollector
 	{
 	public:
