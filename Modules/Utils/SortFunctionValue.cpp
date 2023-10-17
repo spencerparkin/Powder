b2952be43bca9039d787d9e6ba1d809554d032bf
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2023-10-16 20:51:41 -0600

    This seems better.  Still not yet tested.

--- Modules/Utils/SortFunctionValue.cpp
+++ Modules/Utils/SortFunctionValue.cpp
@@ -41,18 +41,141 @@ SortFunctionValue::SortFunctionValue()
 		return false;
 	}
 
-	// TODO: I'm not happy about our inability to do proper error-handling here.
-	listValue->SortWithPredicate([=, &listValue, &predicateValue, &context, &error](const Powder::LinkedList<GC::Reference<Value, false>>::Node* nodeA, const Powder::LinkedList<GC::Reference<Value, false>>::Node* nodeB) -> bool {
-		GC::Reference<Value, true> returnValueRef;
-		GC::Reference<Powder::ListValue, true> predicateArgListValueRef(new Powder::ListValue());
-		predicateArgListValueRef.Get()->PushRight(const_cast<Powder::Value*>(nodeA->value.Get()));
-		predicateArgListValueRef.Get()->PushRight(const_cast<Powder::Value*>(nodeB->value.Get()));
-		this->CallScriptFunction(predicateValue, predicateArgListValueRef, returnValueRef, context, error);
-		return returnValueRef.Get()->AsBoolean();
-	});
+	SortedArrayQueue sortedArrayQueue;
+	for (int i = 0; i < (signed)listValue->Length(); i++)
+	{
+		SortedArray* sortedArray = new SortedArray();
+		sortedArray->push_back((*listValue)[i]);
+		sortedArrayQueue.push_back(sortedArray);
+	}
+
+	SortedArrayQueue::iterator iter;
+	while (sortedArrayQueue.size() > 1)
+	{
+		iter = sortedArrayQueue.begin();
+		SortedArray* sortedArrayA = *iter;
+		sortedArrayQueue.erase(iter);
+
+		iter = sortedArrayQueue.begin();
+		SortedArray* sortedArrayB = *iter;
+		sortedArrayQueue.erase(iter);
+
+		SortedArray* sortedArray = this->MergeSortedArrays(sortedArrayA, sortedArrayB, predicateValue, context, error);
+
+		delete sortedArrayA;
+		delete sortedArrayB;
+
+		if (!sortedArray)
+		{
+			error.Add("Merge of sorted arrays failed.");
+
+			while (sortedArrayQueue.size() > 0)
+			{
+				iter = sortedArrayQueue.begin();
+				delete* iter;
+				sortedArrayQueue.erase(iter);
+			}
+
+			return false;
+		}
+
+		sortedArrayQueue.push_back(sortedArray);
+	}
+
+	iter = sortedArrayQueue.begin();
+	SortedArray* sortedArray = *iter;
+	sortedArrayQueue.erase(iter);
+
+	for (int i = 0; i < (signed)sortedArray->size(); i++)
+		listValue->PushRight((*sortedArray)[i]);
+
+	for (int i = 0; i < (signed)sortedArray->size(); i++)
+	{
+		GC::Reference<Value, true> valueRef;
+		if (!listValue->PopLeft(valueRef, error))
+			return false;
+	}
+
+	delete sortedArray;
 
 	if (error.GetCount() > 0)
 		return false;
+
+	return true;
+}
+
+SortFunctionValue::SortedArray* SortFunctionValue::MergeSortedArrays(const SortedArray* sortedArrayA, const SortedArray* sortedArrayB, Powder::AddressValue* predicateValue, Powder::CppCallingContext& context, Powder::Error& error)
+{
+	SortedArray* sortedArray = new SortedArray();
+
+	int i = 0;
+	int j = 0;
+
+	while (i < (signed)sortedArrayA->size() || j < (signed)sortedArrayB->size())
+	{
+		if (i < (signed)sortedArrayA->size() && j < (signed)sortedArrayB->size())
+		{
+			Value* valueA = (*sortedArray)[i];
+			Value* valueB = (*sortedArray)[j];
+
+			bool compareResult = false;
+
+			if (!this->CompareValues(valueA, valueB, compareResult, predicateValue, context, error))
+			{
+				delete sortedArray;
+				return nullptr;
+			}
+
+			if (compareResult)
+			{
+				sortedArray->push_back(valueA);
+				i++;
+			}
+			else
+			{
+				sortedArray->push_back(valueB);
+				j++;
+			}
+		}
+		else if (i < (signed)sortedArrayA->size())
+		{
+			sortedArray->push_back((*sortedArrayA)[i++]);
+		}
+		else if (j < (signed)sortedArrayB->size())
+		{
+			sortedArray->push_back((*sortedArrayB)[j++]);
+		}
+	}
+
+	return sortedArray;
+}
+
+bool SortFunctionValue::CompareValues(const Powder::Value* valueA, const Powder::Value* valueB, bool& compareResult, Powder::AddressValue* predicateValue, Powder::CppCallingContext& context, Powder::Error& error)
+{
+	// Note that I almost added some memoization to this subroutine, but assuming each
+	// pointer in the list is unique, I don't think that we ever perform a redundant
+	// comparison operation here, unless I'm mistaken.  Regardless, at some point, the
+	// overhead in calling the predicate should be outweighed by trying to do a look-up
+	// in a memoization map, for certain simple predicates.
+
+	compareResult = false;
+
+	GC::Reference<Powder::ListValue, true> predicateArgListValueRef(new Powder::ListValue());
+	GC::Reference<Powder::Value, true> returnValueRef;
+
+	predicateArgListValueRef.Get()->PushRight(const_cast<Powder::Value*>(valueA));
+	predicateArgListValueRef.Get()->PushRight(const_cast<Powder::Value*>(valueB));
+
+	if (!this->CallScriptFunction(predicateValue, predicateArgListValueRef, returnValueRef, context, error))
+		return false;
+
+	if (!returnValueRef.Get())
+	{
+		error.Add("Expected predicate to return a boolean value.");
+		return false;
+	}
+
+	compareResult = returnValueRef.Get()->AsBoolean();
 
 	return true;
 }
--- Modules/Utils/SortFunctionValue.h
+++ Modules/Utils/SortFunctionValue.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CppFunctionValue.h"
+#include "AddressValue.h"
 
 class SortFunctionValue : public Powder::CppFunctionValue
 {
@@ -9,4 +10,12 @@ public:
 	virtual ~SortFunctionValue();
 
 	virtual bool Call(Powder::ListValue* argListValue, GC::Reference<Powder::Value, true>& returnValueRef, Powder::CppCallingContext& context, Powder::Error& error) override;
+
+private:
+
+	typedef std::vector<Powder::Value*> SortedArray;
+	typedef std::list<SortedArray*> SortedArrayQueue;
+
+	SortedArray* MergeSortedArrays(const SortedArray* sortedArrayA, const SortedArray* sortedArrayB, Powder::AddressValue* predicateValue, Powder::CppCallingContext& context, Powder::Error& error);
+	bool CompareValues(const Powder::Value* valueA, const Powder::Value* valueB, bool& compareResult, Powder::AddressValue* predicateValue, Powder::CppCallingContext& context, Powder::Error& error);
 };
